32427793d24c34c0e3003bbdff798eaf86224b60
JoyceFSantoAntonio <joyce.fatima@lojasantoantonio.com.br>
2024-01-04 21:20:08 -0300

    feat: new route create get

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,7 +6,31 @@ import { FastifyInstance } from 'fastify'
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select('*')
-    return transactions
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = knex('transactions').sum('amount', { as: 'amount' }).first()
+
+    return {
+      summary,
+    }
   })
 
   app.post('/', async (request, reply) => {
@@ -18,10 +42,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     const { title, amount, type } = schemaTransaction.parse(request.body)
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      sessionId,
     })
 
     // HTTP Code 201: Created
